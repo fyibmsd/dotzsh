0a74aabd5ee0decd6a9e53e901cefeb7a17d5ff6
fyibmsd <fyibmsd@gmail.com>
2020-06-23 10:18:39 +0800

    Things went wrong...

--- init.sh
+++ init.sh
@@ -11,10 +11,9 @@ set -x
 rm -rf ~/.zsh ~/.oh-my-zsh ~/.zshrc
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# check os release
-os=$(curl -sL https://raw.githubusercontent.com/fyibmsd/server-scripts/master/os.sh | bash -)
+
 # install plugins
-git clone --depth=1 -b $os https://github.com/fyibmsd/dotzsh ~/.zsh
+git clone --depth=1 -b $BRANCH https://github.com/fyibmsd/dotzsh ~/.zsh
 
 cd ~/.zsh
 # local config
